2865cf61abe854463e1c13a17e7d71366ec21f9f
VladislavBaranov <31881026+VladislavBaranov@users.noreply.github.com>
2017-09-28 18:14:36 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
+module.exports = GuessingGame;
+
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.prevGuess = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.prevGuess = Math.ceil((this.max - this.min) / 2) + this.min;
+        return this.prevGuess;
     }
 
     lower() {
-
+        this.max = this.prevGuess;
     }
 
     greater() {
-
+        this.min = this.prevGuess;
     }
 }
 
